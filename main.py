d7a7764d72d2beb973939cd8535589c5465601bc
Grishka3002 <grishapinchuk2013@gmail.com>
2021-10-10 12:06:03 -0500

    -

--- main.py
+++ main.py
@@ -6,5 +6,18 @@ def summ(n):
         s = s + a
     print(s)
 
-h = int(input('Введите количство слагаемых'))
-summ(h)
+def umn(n):
+    i = 0
+    s = 1
+    for i in range(n):
+        a = int(input())
+        s = s * a
+    print(s)
+
+d = input("Выберите действие, которое хотите совершить. 1 - умножение, 2 - сложение.")
+if d == "1":
+    h = int(input('Введите количство множителей'))
+    umn(h)
+else:
+    h = int(input('Введите количство слагаемых'))
+    summ(h)
